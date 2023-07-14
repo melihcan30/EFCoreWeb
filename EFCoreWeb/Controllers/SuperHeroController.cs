3ac4b5cacfd6a34152be8f51956d67d5ea59d399
Melihcan Serbest <canmelih.free3@gmail.com>
2023-07-14 14:16:35 +0300

    GET a Single Hero

--- EFCoreWeb/Controllers/SuperHeroController.cs
+++ EFCoreWeb/Controllers/SuperHeroController.cs
@@ -8,22 +8,41 @@ namespace EFCoreWeb.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
-        {
-            var superHeroes = new List<SuperHero>
+        private static List<SuperHero> superHeroes = new List<SuperHero>
             {
-                new SuperHero 
-                { 
-                    Id = 1, 
-                    Name = "Spider Man", 
-                    FirstName = "Peter", 
-                    LastName = "Parker", 
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
                     Place = "New York City"
+                },
+                new SuperHero
+                {
+                    Id = 2,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Malibu"
                 }
             };
 
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroes()
+        {
             return Ok(superHeroes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> GetSingleHeroe(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero == null)
+            {
+                return NotFound("No superhero found");
+            }
+            return Ok(hero);
         }
     }
 }
